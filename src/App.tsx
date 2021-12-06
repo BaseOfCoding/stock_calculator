480a3c709b1e3cbd3a0fd2409b06bcac42b00598
이정민 <lee@ijeongmin-ui-MacBookPro.local>
2021-12-06 17:47:29 +0900

    평단 계산 css 완료

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,20 @@ function App() {
   return (
     <div>
       <Header />
+      {/*div로 감싼 후, 먼저 Header 컴포넌트를 실행해서, 메뉴 헤더를 보이게 함. App.tsx에 많은 css가 보이면 가독성이 떨어지기에, Header는 Header로 따로 빼놓고 사용.*/}
       <div id="body">
         <Switch>
           <Redirect exact path="/" to="/stockavarage" />
-          <Route path="/stockavarage" component={StockAvarage} />
-          <Route path="/stockprofit" component={StockProfit} />
+          <Route exact path="/stockavarage" component={StockAvarage} />
+          <Route exact path="/stockprofit" component={StockProfit} />
         </Switch>
+        {/* body라는 id를 가진 div로 나누어, 주식 평단계산과, 주식 수익계산 뷰를 보여주게 하고, react-router-dom에서 제공하는 Switch를 이용해서, 각 메뉴를 누를 때마다 url을 다르게 해서, 평단 계산 또는 수익 계산 뷰를 보여주게 한다. */}
       </div>
       <div id="footer">
         <span className="footer-made-name">Since 2021 developer : Jeong min Lee</span>
         <span className="footer-contact-us">Contact US : jmboc.0501@gmail.com</span>
       </div>
+      {/* footer쪽은, 언제 누가 만들었는 지와 개발자의 이메일을 작성 */}
     </div>
   );
 }
--- src/pages/StockAvarage/index.tsx
+++ src/pages/StockAvarage/index.tsx
@@ -1,10 +1,86 @@
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
+import '../StockAvarage/index.css';
 
 const StockAvarage = () => {
+  const current_stock_style = {
+    background: 'orange',
+  };
+
+  const buy_stock_style = {
+    background: 'green',
+  };
+
+  const result_stock_style = {
+    background: 'skyblue',
+  };
+
   return (
     <div>
-      <h1>HI</h1>
-      <Button>HEELOOO</Button>
+      <div className="body">
+        <div className="side" />
+        <div className="center">
+          <div className="title">
+            <i className="fas fa-calculator"></i>
+            <h2>주식(코인) 평단 계산</h2>
+          </div>
+          {/* current_stock ( 현재 평단가 그룹 ) */}
+          <div style={current_stock_style} className="stock-box">
+            <h3>현재 평단가</h3>
+            <div className="stock-box-child">
+              <div className="input-group">
+                <input placeholder="현재 평단" type="text" />
+                <div>원</div>
+              </div>
+              <div className="input-group">
+                <input placeholder="보유 주식 수" type="text" />
+                <div>원</div>
+              </div>
+            </div>
+            <div className="input-group-allPrice">
+              <input placeholder="현재 총 매입가" type="text" disabled />
+              <div>원</div>
+            </div>
+          </div>
+          {/* buy_stock ( 구매 평단가 그룹 ) */}
+          <div style={buy_stock_style} className="stock-box">
+            <h3>현재 평단가</h3>
+            <div className="stock-box-child">
+              <div className="input-group">
+                <input placeholder="현재 평단" type="text" />
+                <div>원</div>
+              </div>
+              <div className="input-group">
+                <input placeholder="보유 주식 수" type="text" />
+                <div>원</div>
+              </div>
+            </div>
+            <div className="input-group-allPrice">
+              <input placeholder="현재 총 매입가" type="text" disabled />
+              <div>원</div>
+            </div>
+          </div>
+          {/* result_stock ( 결과 평단가 그룹 ) */}
+          <div style={result_stock_style} className="stock-box">
+            <h3>현재 평단가</h3>
+            <div className="stock-box-child">
+              <div className="input-group">
+                <input placeholder="현재 평단" type="text" />
+                <div>원</div>
+              </div>
+              <div className="input-group">
+                <input placeholder="보유 주식 수" type="text" />
+                <div>원</div>
+              </div>
+            </div>
+            <div className="input-group-allPrice">
+              <input placeholder="현재 총 매입가" type="text" disabled />
+              <div>원</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="side" />
+      </div>
     </div>
   );
 };
